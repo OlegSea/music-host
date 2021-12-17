78e2d457823f008e5c64ea822e7e22bebbf00244
OlegSea <ta.alexashow@ya.ru>
2021-12-17 15:46:03 +0300

    bruh

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ import websockets
 import re
 from multiprocessing import Process
 
-music_folder = '/e/Files/Music'
+music_folder = '/home/olegsea/Music'
 
+should_break = False
 
 async def handler(websocket):
     async for message in websocket:
+        global should_break
         if re.search('^play (.*)$', message):
+            should_break = True
             song = music_folder + re.findall('^play (.*)$', message)[0]
 
             raw_data = miniaudio.decode_file(song)
             for i in range(0, len(raw_data.samples), raw_data.sample_rate):
+                if i == 0:
+                    should_break = False
+                if should_break:
+                    break
                 await websocket.send(bytes(raw_data.samples[i:i+raw_data.sample_rate]))
 
 
